188723beeea4bc1800f157234f3905c134309953
PedroBelo1551 <70293675+PedroBelo1551@users.noreply.github.com>
2020-09-14 22:49:07 -0300

    Add files via upload

---
+++ TIS3---Nondas-master/src/Entidades/Cliente.java
@@ -0,0 +1,75 @@
+package Entidades;
+
+import java.io.Serializable;
+import java.util.Random;
+
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	//private int codCli;
+	private String nome;
+	private long cpfCnpj;
+	private String email;
+	private String telefone;
+	Random random = new Random();
+
+	public Cliente(){}
+
+	public Cliente(long cpfCnpj, String nome, String email, String telefone) {
+
+		setCpfCnpj(cpfCnpj);
+		setNome(nome);
+		setEmail(email);
+		setTelefone(telefone);
+		//setCodCli();
+
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public long getCpfCnpj() {
+		return cpfCnpj;
+	}
+
+	public void setCpfCnpj(long f) {
+		this.cpfCnpj = f;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/*
+	public int getCodCli() {
+		return codCli;
+	}
+
+	public void setCodCli() {
+		this.codCli = random.nextInt(1000);
+	}
+	 */
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public void print() {
+		System.out.println(this.nome + this.cpfCnpj + this.email + this.telefone);
+	}
+
+
+}//class cliente
---
+++ TIS3---Nondas-master/src/Main/Main.java
@@ -0,0 +1,97 @@
+package Main;
+
+import java.awt.event.*; 
+import javax.swing.*;
+
+import DAO.CadastroClienteDAO;
+import Entidades.Cliente;
+import Telas.TelaCadastros;
+
+
+
+public class Main{
+
+
+	public static void main(String[] args) {
+		try {
+			
+
+			new TelaPrincipal();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}//main
+
+class TelaPrincipal extends JFrame{
+	// JFrame 
+	static JFrame f; 
+
+	// JButton 
+	static JButton btCad, btLan, btRel, btSai; 
+
+	// label to display text 
+	static JLabel l; 
+
+	Box box;
+
+	public TelaPrincipal() {
+		// create a new frame with text field and button 
+		f = new JFrame("Nondas Celulares"); 
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// create a label to display text 
+		l = new JLabel("Selecione a opção desejada:"); 
+
+		// create a new buttons 
+		btCad = new JButton("Cadastros"); 
+		btLan = new JButton("Lançamentos"); 
+		btRel = new JButton("Relatórios"); 
+		btSai = new JButton("Sair");
+
+		btCad.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new TelaCadastros();
+
+			}
+		});
+
+		btSai.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				f.dispose();
+				System.exit(0);
+
+			}
+		});
+
+		// create a panel to add buttons 
+		JPanel p = new JPanel(); 
+		box = Box.createVerticalBox();
+		// add buttons and textfield to panel 
+		box.add(l); 
+		box.add(btCad); 
+		box.add(btLan); 
+		box.add(btRel); 
+		box.add(btSai);
+
+
+		p.add(box);
+
+		// add panel to frame 
+		f.add(p); 
+
+		// set the size of frame 
+		f.setSize(300, 300); 
+
+		f.show();
+
+	}
+}//home screen
+
+
+
