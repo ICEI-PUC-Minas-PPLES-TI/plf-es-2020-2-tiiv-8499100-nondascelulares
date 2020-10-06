11a49fd2dc0adfe3fae1b5296267c5f0197b2827
Sunimar Junior <43272283+sunimar@users.noreply.github.com>
2020-10-05 23:16:01 -0300

    Update OrdemVenda.java

--- Codigo/src/br/tis/entidades/OrdemVenda.java
+++ Codigo/src/br/tis/entidades/OrdemVenda.java
@@ -1,20 +1,26 @@
 package br.tis.entidades;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
+//import java.util.Date;
 import java.util.List;
+import java.sql.Date;
+import java.time.LocalDate;
+import br.tis.dao.EstoqueDAO;
 
-public class OrdemVenda  {
-
+public class OrdemVenda {
 
 	private Date data;
 	private long codVenda;
 	public List<Produto> produtos;
 	private float valorTotal;
+	private Estoque lancamento = new Estoque();
+	private EstoqueDAO estoqueDao = new EstoqueDAO(lancamento);
 
+	@SuppressWarnings("deprecation")
 	public OrdemVenda() {
-		data = new Date();
+		data = new Date(0, 0, 0);
 		codVenda = 0;
 		produtos = new ArrayList<Produto>();
 		valorTotal = 0;
@@ -32,14 +38,21 @@ public class OrdemVenda  {
 		return this.produtos;
 	}
 	
-	public boolean produtoExiste(long id) {
+	public List<Produto> getEstoque(){
+		List<Produto> estoque = null;
 		
+		//chamar metodo do estoque
+		
+		return estoque;
+	}
+
+	public boolean produtoExiste(long id) {
+
 		for(Produto i : this.produtos) {
 			if(i.getIdProduto() == id) {
 				return true;
 			}
 		}
-		
 		return false;
 	}
 
@@ -53,7 +66,7 @@ public class OrdemVenda  {
 
 	public void setValorTotal() {
 		float temp=0;
-	
+
 		this.valorTotal = temp;
 	}
 
@@ -68,6 +81,28 @@ public class OrdemVenda  {
 	public String toString() {
 		String tmp = new SimpleDateFormat("EEE, dd 'de' MMM 'de' yyyy, HH:mm").format(this.data);
 		return tmp + ";" + "'" + this.codVenda + ";" +  this.valorTotal;
+	}
+
+	public void carrinhoEstoque() {
+		
+		for(Produto p: produtos) {
+			addEstoque(p);
+		}
+		
+	}
+
+	public void addEstoque(Produto prod) {
+		
+		lancamento.setTipoLancamento(TipoLancamento.valueOf("SAIDA"));
+		lancamento.setDataLancamento(data.valueOf(LocalDate.now()));//mexer na data
+		lancamento.setDocumento("546546");
+		lancamento.setIdproduto(prod.getIdProduto());
+		lancamento.setNomeProduto(prod.getDescricao());
+		lancamento.setCustoUnitario(50);
+		lancamento.setPrecoVendaUnitario(15);
+		lancamento.setQuantidade(5);
+
+		estoqueDao.add();
 	}
 
 }
